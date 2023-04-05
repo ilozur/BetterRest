75773b10d2b47622ee029c6cb60b1ceb2f628179
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-05 22:29:35 +0300

    Selecting dates and times with DatePicker

--- BetterRest/Overview.swift
+++ BetterRest/Overview.swift
@@ -10,10 +10,24 @@ import SwiftUI
 struct Overview: View {
     
     @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
     
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        VStack {
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                .padding(25)
+            DatePicker("Please, enter a date", selection: $wakeUp, in: Date.now..., displayedComponents: .date)
+                .labelsHidden()
+            DatePicker("Please, enter a time", selection: $wakeUp, in: Date.now..., displayedComponents: .hourAndMinute)
+                .labelsHidden()
+        }
     }
+    
+//    func exampleData() {
+//        let tomorrow = Date.now.addingTimeInterval(86400)
+//        let range = Date.now...tomorrow
+//    }
+    
 }
 
 struct Overview_Previews: PreviewProvider {
