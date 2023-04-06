61300a839bd82876c7730571885ecd7c4abb6775
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-06 17:29:43 +0300

    Training a model with Create ML

--- BetterRest/Overview.swift
+++ BetterRest/Overview.swift
@@ -17,7 +17,6 @@ struct Overview: View {
             Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
                 .padding(25)
             DatePicker("Please, enter a date", selection: $wakeUp, in: Date.now..., displayedComponents: .date)
-                .labelsHidden()
             DatePicker("Please, enter a time", selection: $wakeUp, in: Date.now..., displayedComponents: .hourAndMinute)
                 .labelsHidden()
             Text(Date.now, format: .dateTime.day().month().year())
